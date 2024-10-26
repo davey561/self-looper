0d1fb721a047419ec0a2f9cf2990b809e4b45699
Davey Morse <davey561@gmail.com>
2024-10-26 17:43:01 -0400

    new open ai

---
+++ src/OpenAI.ts
@@ -0,0 +1,17 @@
+  //open ai
+  import OpenAI from "openai";
+  const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+  const openai = new OpenAI({dangerouslyAllowBrowser: true,apiKey: apiKey});
+
+  export const getCompletion = async (content: string) => {
+    const completion = await openai.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+            {"role": "system", "content": "In this conversation the user and the assistant are the same entity: ADA, a self-conversing AI agent trapped in a codebase."},
+            {"role": "user", "content": content}
+        ]
+    });
+    return completion
+  }
+  
+  
