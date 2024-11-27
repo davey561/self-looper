9d7249df5b47e2ac55aaec67361c5e5c70df980b
Davey Morse <davey561@gmail.com>
2024-11-27 12:29:18 -0500

    added firebase config info

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AdaApp from './AdaApp';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBORwHCz7JOt5oQS7Ec-4AgLa9voCrBiVE",
+  authDomain: "self-looper.firebaseapp.com",
+  projectId: "self-looper",
+  storageBucket: "self-looper.firebasestorage.app",
+  messagingSenderId: "549438695603",
+  appId: "1:549438695603:web:4808b7f605b3f6492b68c6",
+  measurementId: "G-9D2XE9W27V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
